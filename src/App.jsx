8977e20931a0d6d7c035aec614a07a0a3f1c43e7
Nishimurasyota <59325211+Nishimurasyota@users.noreply.github.com>
2022-01-05 20:39:01 +0900

    fix usercard_file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { UserCard } from "./components/oraganisms/user/UserCard";
 import { SearchiInput } from "./molecules/SearchInput";
 import "./styles.css";
+
+const user = {
+  name: "syota",
+  image: "https://source.unsplash.com/LATYeZyw88c",
+  email: "aaa@com",
+  phone: "111-222-3333",
+  company: {
+    name: "testcampany"
+  },
+  website: "https://google.com"
+};
 
 export const App = () => {
   return (
@@ -10,6 +22,7 @@ export const App = () => {
       <SecondaryButton>ボタン</SecondaryButton>
       <br />
       <SearchiInput />
+      <UserCard user={user} />
     </div>
   );
 };
