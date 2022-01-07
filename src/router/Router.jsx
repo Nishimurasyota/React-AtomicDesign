1d2b2ff3aa578cdab6439b0dd9f05ab0546d94a6
Nishimurasyota <59325211+Nishimurasyota@users.noreply.github.com>
2022-01-07 21:24:58 +0900

    fix Router_file Router-dom version6.3

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Top } from "../components/pages/Top";
 import { Users } from "../components/pages/Users";
 import { DefaultLayout } from "../components/templates/DefaultLayout";
@@ -7,18 +7,25 @@ import { HeaderOnly } from "../components/templates/HeaderOnly";
 export const Router = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <DefaultLayout>
-            <Top />
-          </DefaultLayout>
-        </Route>
-        <Route path="/users">
-          <HeaderOnly>
-            <Users />
-          </HeaderOnly>
-        </Route>
-      </Switch>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <DefaultLayout>
+              <Top />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path="/users"
+          element={
+            <HeaderOnly>
+              <Users />
+            </HeaderOnly>
+          }
+        />
+      </Routes>
     </BrowserRouter>
   );
 };
